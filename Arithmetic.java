383366dd67e2ed743a5c4cfeca495e5e7e676ac2
siris2204 <149220435+siris2204@users.noreply.github.com>
2024-02-24 15:23:21 +0530

    Arithmetic operations using java

---
+++ Arithmetic.java
@@ -0,0 +1,12 @@
+class Arithmetic
+{
+   public static void main(String[] args)
+   {
+      int a=5,b=25;
+      System.out.println("Sum is: "+(a+b));
+      System.out.println("Difference is: "+(a-b));
+      System.out.println("Product is: "+(a*b)); 
+      System.out.println("Quotient is: "+(a/b));
+      System.out.println("Remainder is: "+(a%b));
+   }
+}     
