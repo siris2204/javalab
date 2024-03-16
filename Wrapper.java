46aa5d95826dd4e31c738f915afbdcd715c268f9
siris2204 <149220435+siris2204@users.noreply.github.com>
2024-03-16 14:49:19 +0530

    Wrapper class
    Program using wrapper classes in java

---
+++ Wrapper.java
@@ -0,0 +1,9 @@
+class Wrapper
+{  
+    public static void main(String[] p)
+    {    System.out.println("Welcome "+p[0]);
+         int a= Integer.parseInt(p[1]);
+         int b= Integer.parseInt(p[2]);
+         System.out.println(a+b);
+     }
+}
