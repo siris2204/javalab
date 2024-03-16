4200c3f1d4cf5e2b2f33eaee8b345955da5fa196
siris2204 <149220435+siris2204@users.noreply.github.com>
2024-03-16 14:26:18 +0530

    Scanner class
    Basic program using scanner class in java

---
+++ Scan.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Scan
+{
+    public static void main(String[] a)
+    {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter you name");
+       String name= sc.nextLine();
+       System.out.println("WELCOME "+name);
+     }
+}
