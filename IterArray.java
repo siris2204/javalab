d308ab72ce370076788b6acb2ecfd2b142af175d
siris2204 <149220435+siris2204@users.noreply.github.com>
2024-03-28 11:54:43 +0530

    java program to iterate an array
    java program to create an array and initialise elements in it and traverse and print the elements of that array.

---
+++ IterArray.java
@@ -0,0 +1,9 @@
+public class IterArray{
+    public static void main(String[] args){
+        int[] arr={1,2,4,5,6,7};
+        System.out.println("Elements are:");
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
