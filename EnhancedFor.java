8c00104fa0df2920c9f1a19cd713e24c39c64de7
siris2204 <149220435+siris2204@users.noreply.github.com>
2024-03-30 15:20:50 +0530

    Java program to illustrate Enhanced for loop

---
+++ EnhancedFor.java
@@ -0,0 +1,11 @@
+class EnhancedFor
+{
+    public static void main(String[] args)
+    {
+        int[] marks={10,20,30,40,50};
+        for(int i:marks)
+        {
+            System.out.println("marks are "+i);
+        }
+    }
+}
