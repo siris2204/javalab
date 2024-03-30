54a28a21c314c98fbde9e15188d695e3eacafef9
siris2204 <149220435+siris2204@users.noreply.github.com>
2024-03-30 14:39:37 +0530

    Java program to display String concatenation.

---
+++ Concatenate.java
@@ -0,0 +1,14 @@
+class Concatenate
+{
+    public static void main(String[] args)
+    {
+        String first="java ";
+        String second="lab";
+        String complete= first.concat(second);
+        System.out.println(complete);
+        String name="james ";
+        String lname="gosling";
+        String auth=name+lname;
+        System.out.println(auth);
+    }
+}
