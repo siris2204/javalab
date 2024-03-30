8a90cd3bc7cf8342b39cff8ee2ff8472475ebe0f
siris2204 <149220435+siris2204@users.noreply.github.com>
2024-03-30 15:19:35 +0530

    Java program to create a string object and compare

---
+++ StringCompare1.java
@@ -0,0 +1,15 @@
+class StringCompare1
+{
+    public static void main(String[] args)
+    {
+        String sch= new String("python");
+        if(sch.equals("java"))
+        {
+            System.out.println("patience is needed");
+        }
+        else
+        {
+            System.out.println("dont waste time");
+        }
+    }
+}
