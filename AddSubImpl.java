fb5571d5f65f0bb8d58d5a1494232bf6b0492a7c
siris2204 <149220435+siris2204@users.noreply.github.com>
2024-04-20 15:28:05 +0530

    Java program using user defined package arithmetic
    This program uses a user defined package and creates objects of the classes present in that package

---
+++ AddSubImpl.java
@@ -0,0 +1,10 @@
+import arithematic.*;
+public class AddSubImpl{
+public static void main(String[] args)
+{
+    Addition a1= new Addition(3,4);
+    a1.sum();
+    Subtraction s1= new Subtraction(5,2);
+    s1.difference();
+}
+}
