0e3d6289495ed73654828bdee4e5d35349f2650d
siris2204 <149220435+siris2204@users.noreply.github.com>
2024-04-20 15:24:43 +0530

    Java package arithmetic
    this is a package with addition class which returns the sum of two numbers

---
+++ Addition.java
@@ -0,0 +1,14 @@
+package arithematic;
+public class Addition
+{   
+    private int d1,d2;
+    public Addition(int n1, int n2)
+    {
+        this.d1=n1;
+        this.d2=n2;
+    }
+    public void sum()
+    {
+        System.out.println("sum of the given numbers is"+(d1+d2));
+    }
+}
